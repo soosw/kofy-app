7a74303180421c01b4c981b537ca5811e717b94e
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-10-03 12:22:10 -0600

    Initial Commit

---
+++ Kofy/KofyApp.swift
@@ -0,0 +1,17 @@
+//
+//  KofyApp.swift
+//  Kofy
+//
+//  Created by Diego Gutierrez on 03/10/23.
+//
+
+import SwiftUI
+
+@main
+struct KofyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
