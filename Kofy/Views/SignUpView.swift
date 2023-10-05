795027fa4549990ba73a84a2e6d6f2aef5c5406d
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-10-04 22:18:22 -0600

    🔑 Added Sign Up View

---
+++ Kofy/Views/SignUpView.swift
@@ -0,0 +1,112 @@
+//
+//  SignUpView.swift
+//  Kofy
+//
+//  Created by Diego Gutierrez on 04/10/23.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    @State private var username: String = ""
+    @State private var mail: String = ""
+    @State private var password: String = ""
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Color("BackgroundColor")
+                    .ignoresSafeArea()
+                
+                VStack {
+                    VStack {
+                        Image("KofyLogo")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 170)
+                        Text("KOFY")
+                            .font(Font.custom("ZenTokyoZoo-Regular", size: 50))
+                            .foregroundStyle(.white)
+                    }
+                    .padding([.top], 50)
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 30) {
+                        VStack(spacing: 15) {
+                            ZStack {
+                                Color(red: 0.16, green: 0.16, blue: 0.16)
+                                    .cornerRadius(10)
+                                TextField("Usuario", text: $username)
+                                    .padding()
+                            }
+                            .frame(height: 60)
+                            
+                            ZStack {
+                                Color(red: 0.16, green: 0.16, blue: 0.16)
+                                    .cornerRadius(10)
+                                TextField("Correo", text: $mail)
+                                    .keyboardType(.emailAddress)
+                                    .padding()
+                            }
+                            .frame(height: 60)
+                            
+                            ZStack {
+                                Color(red: 0.16, green: 0.16, blue: 0.16)
+                                    .cornerRadius(10)
+                                SecureField("Contraseña", text: $password)
+                                    .padding()
+                            }
+                            .frame(height: 60)
+                        }
+                        .frame(width: geometry.size.width * 0.75)
+                        
+                        Button {
+                            print("Registrarme")
+                        } label: {
+                            Text("Registrarme")
+                                .frame(width: geometry.size.width / 2.5)
+                        }
+                        .padding()
+                        .background(LinearGradient(gradient: Gradient(colors: [Color(red: 0.694, green: 0.376, blue: 0.941), Color(red: 0.533, green: 0.49, blue: 1)]), startPoint: .leading, endPoint: .trailing))
+                        .cornerRadius(.infinity)
+                        .foregroundStyle(.white)
+                        .bold()
+                        .overlay(
+                            RoundedRectangle(cornerRadius: .infinity)
+                                .stroke(.white, lineWidth: 3)
+                        )
+                    }
+                    
+                    VStack {
+                        Color(red: 0.313, green: 0.313, blue: 0.313)
+                            .frame(width: geometry.size.width / 3, height: 4)
+                            .cornerRadius(.infinity)
+                    }
+                    .padding([.top, .bottom], 10)
+                    
+                    Button {
+                        print("Registrarme")
+                    } label: {
+                        HStack {
+                            Image(systemName: "apple.logo")
+                                .font(.title)
+                            Text("Registrarme")
+                                .frame(width: geometry.size.width / 3.5)
+                        }
+                        .padding()
+                    }
+                    .background(.black)
+                    .cornerRadius(.infinity)
+                    .foregroundStyle(.white)
+                    .bold()
+                }
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+        }
+    }
+}
+
+#Preview {
+    SignUpView()
+}
