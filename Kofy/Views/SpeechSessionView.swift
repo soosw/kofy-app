74a3b00694ef2feb3b7318a1e2f2834c9c7b82af
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-10-11 22:46:40 -0600

    🚧 Initial speech recognition

---
+++ Kofy/Views/SpeechSessionView.swift
@@ -0,0 +1,21 @@
+//
+//  SpeechSessionView.swift
+//  Kofy
+//
+//  Created by Diego Gutierrez on 11/10/23.
+//
+
+import SwiftUI
+import Speech
+import Foundation
+import AVFoundation
+
+struct SpeechSessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpeechSessionView()
+}
