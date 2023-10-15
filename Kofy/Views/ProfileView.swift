e7ce6e1759b21fc6ab17fa8e677d24c208be9b81
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-10-14 22:47:07 -0600

    Added profile view

--- Kofy/Views/ProfileView.swift
+++ Kofy/Views/ProfileView.swift
@@ -9,7 +9,46 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text("Profile")
+        GeometryReader { geometry in
+            ZStack {
+                Color("BackgroundColor")
+                    .ignoresSafeArea()
+                
+                VStack {
+                    VStack(spacing: 0) {
+                        Color(Color(red: 0.85, green: 0.85, blue: 0.85))
+                            .ignoresSafeArea()
+                            .frame(maxHeight: 0)
+                        HStack {
+                            Text("Tu perfil")
+                                .font(Font.system(size: 35, weight: .bold))
+                                .foregroundStyle(Color(red: 0.278, green: 0.278, blue: 0.278))
+                                .padding([.leading], 30)
+                            Spacer()
+                            Image("cucho")
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                                .frame(width: 70)
+                                .padding([.trailing], 10)
+                        }
+                        .padding(8)
+                        .background(Color(red: 0.85, green: 0.85, blue: 0.85))
+                        .clipShape(
+                            .rect(
+                                bottomLeadingRadius: 20,
+                                bottomTrailingRadius: 20
+                            )
+                        )
+                    }
+                    
+                    Spacer()
+                    
+                }
+            }
+            .ignoresSafeArea(.keyboard)
+            .frame(width: geometry.size.width)
+        }
     }
 }
 
