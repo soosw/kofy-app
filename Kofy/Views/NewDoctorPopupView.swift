8d7ab21d6fc078ac692ad3deb6a726a9655124fb
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-10-14 22:47:44 -0600

    Fixed popups

--- Kofy/Views/NewDoctorPopupView.swift
+++ Kofy/Views/NewDoctorPopupView.swift
@@ -10,6 +10,18 @@ import SwiftUI
 struct NewDoctorPopupView: View {
     @Binding var popupIsShown: Bool
     
+    // Form Inputs
+    @State var doctorName = ""
+    @State var doctorFocus = ""
+    @State var doctorPhone = ""
+    @State var doctorEmail = ""
+    
+    @State var cardSlideOffset = 0
+    
+    func dismissKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
@@ -20,15 +32,24 @@ struct NewDoctorPopupView: View {
                         Text("Nuevo Doctor")
                             .foregroundStyle(.gray)
                             .padding()
+                            .padding([.leading], 8)
                             .font(Font.system(size: 28))
                             .bold()
                             .onChange(of: popupIsShown, { oldValue, newValue in
-                                
+                                if (!newValue) {
+                                    doctorName = ""
+                                    doctorFocus = ""
+                                    doctorPhone = ""
+                                    doctorEmail = ""
+                                } else {
+                                    cardSlideOffset = 0
+                                }
                             })
                         
                         Spacer()
                         
                         Button {
+                            dismissKeyboard()
                             popupIsShown.toggle()
                         } label: {
                             Image(systemName: "x.circle")
@@ -41,10 +62,37 @@ struct NewDoctorPopupView: View {
                     }
                     .foregroundStyle(.black)
                     .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
-                    .padding([.bottom], 10)
+                    
+                    VStack {
+                        TextField("Nombre", text: $doctorName, prompt: Text("Nombre").foregroundStyle(.gray))
+                            .foregroundStyle(.black)
+                            .padding()
+                            .background(Color(red: 0.976, green: 0.976, blue: 0.976))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        TextField("Especialidad", text: $doctorFocus, prompt: Text("Especialidad").foregroundStyle(.gray))
+                            .foregroundStyle(.black)
+                            .padding()
+                            .background(Color(red: 0.976, green: 0.976, blue: 0.976))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        TextField("Teléfono", text: $doctorPhone, prompt: Text("Teléfono").foregroundStyle(.gray))
+                            .foregroundStyle(.black)
+                            .padding()
+                            .background(Color(red: 0.976, green: 0.976, blue: 0.976))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        TextField("Correo", text: $doctorEmail, prompt: Text("Correo").foregroundStyle(.gray))
+                            .foregroundStyle(.black)
+                            .padding()
+                            .background(Color(red: 0.976, green: 0.976, blue: 0.976))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    .padding()
                     
                     Button {
                         // Registar doctor aquí
+                        dismissKeyboard()
                         popupIsShown.toggle()
                     } label: {
                         VStack {
@@ -58,14 +106,39 @@ struct NewDoctorPopupView: View {
                         .background(.blue)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                     }
+                    .padding(.bottom, geometry.safeAreaInsets.bottom)
+                    
+                    
+                    VStack {
+                        EmptyView()
+                    }
+                    .frame(height: 220)
                 }
                 .frame(width: geometry.size.width)
                 .padding(.bottom, geometry.safeAreaInsets.bottom)
                 .background(.white)
                 .clipShape(RoundedCornersShape(radius: 16, corners: [.topLeft, .topRight]))
+                .offset(y: CGFloat(cardSlideOffset))
             }
             .shadow(color: .gray, radius: 15, x: 0,  y: 8)
             .edgesIgnoringSafeArea([.bottom])
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        if (value.translation.height > 0 && popupIsShown) {
+                            cardSlideOffset = Int(value.translation.height)
+                        }
+                        
+                        if value.translation.height > 100 {
+                            dismissKeyboard()
+                            popupIsShown = false
+                        } else if (popupIsShown) {
+                            withAnimation(Animation.easeInOut(duration: 0.2)) {
+                                cardSlideOffset = 0
+                            }
+                        }
+                    }
+            )
         }
     }
 }
