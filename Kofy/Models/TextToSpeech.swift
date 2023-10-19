da6b71c5c7b27c39a9e5ba094df224cf7aca0dc2
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2023-10-19 00:38:15 -0600

    Speech to Text Fixes

--- Kofy/Models/TextToSpeech.swift
+++ Kofy/Models/TextToSpeech.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import AVFoundation
+
+protocol SpeechSynthesizerProviding {
+    func speakText(_ text: String)
+}
+
+final class SpeechSynthesizer: SpeechSynthesizerProviding {
+    let synthesizer = AVSpeechSynthesizer()
+    
+    func speakText(_ text: String) {
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "es-MX")
+        synthesizer.speak(utterance)
+    }
+}
+
+@MainActor
+final class ContentViewModel: ObservableObject {
+    @Published var text: String = ""
+    
+    let speechSynthesizer: SpeechSynthesizerProviding
+    
+    init(speechSynthesizer: SpeechSynthesizerProviding) {
+        self.speechSynthesizer = speechSynthesizer
+    }
+    
+    func textToSpeech() {
+        speechSynthesizer.speakText(self.text)
+    }
+}
